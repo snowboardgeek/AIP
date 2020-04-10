4a9b2dd09bf505ab6957e062e634fc70106ce5d9
Glen Nygaard <glen.nygaard@gmail.com>
2020-04-10 13:48:24 +0200

    Update AIPActions.ps1

--- AIPActions.ps1
+++ AIPActions.ps1
@@ -1,12 +1,35 @@
+
+
 #install module AIp
 Install-Module -Name AIPService
 Update-Module -Name AIPService
 (Get-Module AIPService –ListAvailable).Version
 
-
 #get the credentials to login
 $Cred = Get-Credential
-
 #connect to the azure AD RMS Service
 Connect-AipService -Credential $Cred
 
+Connect-AipService
+
+Get-Command -Module AIPService *Template*
+
+#Get All Templates
+Get-AadrmTemplate
+
+Get-AipServiceTemplate
+
+#Remove a Template (Label)
+Remove-AipServiceTemplate -TemplateID ""
+
+#Create a new template
+# locale identifier for Norway NB and NO Norwegian (Bokmål)	 0414	1044
+#                                        Norwegian (Nynorsk) 0814	2068
+#Create a new template
+$names = @{}
+$names[1033] = "labglen - Glen"
+$descriptions = @{}
+$descriptions[1033] = "hemmelig Hemmelig"
+$r1 = New-AadrmRightsDefinition -DomainName "labglenad.onmicrosoft.com" -Rights "VIEW","EXPORT"
+$r2 = New-AadrmRightsDefinition -EmailAddress "glen.nygaard@gmail.com" -Rights "OWNER"
+Add-AipServiceTemplate -Names $names -Descriptions $Descriptions -LicenseValidityDuration 5 -RightsDefinitions $r1, $r2 -Status Published
